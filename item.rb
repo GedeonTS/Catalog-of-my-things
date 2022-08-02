c70957f8911039a9a0a78dcd71b9fda7632201f8
ericmahare <erikmahare@gmail.com>
2022-08-03 02:22:18 +0300

    Fix linter errors

--- item.rb
+++ item.rb
@@ -1,7 +1,8 @@
 require 'date'
-class Item 
-  attr_accessor :genre, :author, :label 
-  def initialize(source, publish_date, archived = false)
+class Item
+  attr_accessor :genre, :author, :label, :archived
+
+  def initialize(source, publish_date, archived: false)
     @id = Random.rand(1..100)
     @source = source
     @label = label
@@ -9,14 +10,13 @@ class Item
     @archived = archived
   end
 
-  def can_be_archived?() 
+  def can_be_archived?()
     current_date = Date.today
     total_years = current_date.year - @publish_date.year
     total_years > 10
   end
 
-  def move_to_archive() 
+  def move_to_archive()
     @archived = true if can_be_archived?
   end
 end
-
