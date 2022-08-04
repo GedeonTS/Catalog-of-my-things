1b53e8e2a7a0b20afc75de6286123984d7b4ecd3
GedeonTS <gedeontshobohwa1@gmail.com>
2022-08-04 14:33:54 +0200

    Add getters and setters for label

--- item.rb
+++ item.rb
@@ -1,10 +1,10 @@
 require 'date'
 class Item
-  attr_accessor :genre, :author, :label, :archived
+  attr_accessor :archived, :id
+  attr_reader :label, :genre, :author
 
-  def initialize(source, publish_date, archived: false)
-    @id = Random.rand(1..100)
-    @source = source
+  def initialize(publish_date, id = nil, archived: false)
+    @id = id || Random.rand(1..100)
     @label = label
     @publish_date = Date.parse(publish_date)
     @archived = archived
@@ -18,5 +18,20 @@ class Item
 
   def move_to_archive()
     @archived = true if can_be_archived?
+  end
+
+  def genre=(genre)
+    @genre = genre.name
+    genre.items << self unless genre.items.include?(self)
+  end
+
+  def author=(author)
+    @author = { first_name: author.first_name, last_name: author.last_name }
+    author.items << self unless author.items.include?(self)
+  end
+
+  def label=(label)
+    @label = label.name
+    label.items << self unless label.items.include?(self)
   end
 end
