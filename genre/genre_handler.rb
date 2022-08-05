f0d112af4f4f59e65889c2870f41c451f039f1db
ericmahare <erikmahare@gmail.com>
2022-08-05 12:50:25 +0300

    Fetch and add genres functionality

--- genre/genre_handler.rb
+++ genre/genre_handler.rb
@@ -1,38 +1,17 @@
-require 'json'
+require_relative './genre'
 
-module GenresHandler
-  def store_genres(genre)
-    file = './genre/genres.json'
-    File.new('./genre/genres.json', 'w+') unless File.exist?(file)
-    data = []
-    data << {
-      name: genre.name,
-      id: genre.id
-    }
-
-    File.write(file, JSON.generate(data))
-  end
-
-  def load_genres
-    data = []
-    file = './genre/genres.json'
-    return data unless File.exist?(file) && File.read(file) != ''
-
-    JSON.parse(File.read(file)).each do |genre|
-      data << Genre.new(genre['name'], genre['id'])
+module GenreModule
+  def add_genres(genres)
+    genre_names = %w[Pop Gospel Reggae Country]
+    genre_names.each_with_index do |option, index|
+      genres << Genre.new(index, option)
     end
-    data
   end
-
-  def list_genres
-    puts '-' * 50
-    if @genres.empty?
-      puts 'No genre yet!'
-    else
-      puts '📑 Genres list:'
-      @genres.each_with_index do |genre, index|
-        puts "#{index + 1}-[Genre] ID: #{genre.id} | Name: #{genre.name}"
-      end
+  
+  def list_genres(genres)
+    puts 'Genres:'
+    genres.each do |genre|
+      puts "#{genre.id} - #{genre.name}"
     end
   end
 end
