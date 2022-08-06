1487177ab6291af82540fef1aa1fba8fcbb4ddd5
ericmahare <erikmahare@gmail.com>
2022-08-06 15:29:48 +0300

    Add unit testing to games

---
+++ spec/game_spec.rb
@@ -0,0 +1,16 @@
+require 'date'
+require_relative '../games/game'
+
+describe Game do
+  context 'Game class' do
+    it 'should have a multiplayer value' do
+      game = Game.new('Mary', '12/12/2012', '12/12/2012')
+      expect(game.multiplayer).to eq('Mary')
+    end
+
+    it 'should have a last_played date' do
+      game = Game.new('Mary', '12/12/2012', '12/12/2012')
+      expect(game.last_played_at).to eq('12/12/2012')
+    end
+  end
+end
